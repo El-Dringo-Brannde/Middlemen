ec3c72813ca66b84b1e4f8148e695e010f4a1d40
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2019-07-23 18:02:43 -0700

    This might actually work

--- src/Azure/index.ts
+++ src/Azure/index.ts
@@ -10,15 +10,13 @@ import {
 
 const validateSchema = schema => (ctx, input) => {
 	const { error } = Joi.validate(input, schema);
-	return ctx.next(
-		error
-			? {
-					message: `Invalid input, ${error.message}`,
-					details: error.details,
-					input: input
-			  }
-			: null
-	);
+	return error
+		? {
+				message: `Invalid input, ${error.message}`,
+				details: error.details,
+				input: input
+		  }
+		: null;
 };
 class AzureMiddleMen {
 	private middlewareStack: MiddlewareStack[] = [];
@@ -61,50 +59,24 @@ class AzureMiddleMen {
 		context: NextContext & Context,
 		inputs: any,
 		...args: Array<any>
-	) => void) => (context, inputs, ...args) =>
-		this._handle(context, inputs, ...args);
+	) => void) => async (context, inputs, ...args) => {
+		let res;
 
-	private _handle(ctx: NextContext, input, ...args): void {
-		const originalDoneImplementation = ctx.done;
-		const stack = this.middlewareStack;
-		let index = 0;
-		let doneWasCalled = false;
+		for await (let layer of this.middlewareStack) {
+			//@ts-ignore
+			res = await layer.func(context, inputs, ...args);
+			if (res instanceof Error) break;
+		}
 
-		ctx.done = (...params: Array<any>) => {
-			if (doneWasCalled) return;
-			doneWasCalled = true;
-			originalDoneImplementation(...params);
+		const exit = () => {
+			context.res = {
+				status: 403,
+				body: res.toString()
+			};
+			context.done();
 		};
-
-		ctx.next = async (err?) => {
-			try {
-				const layer = stack[index++];
-				// No more layers to evaluate, Call done and exit
-				if (!layer) return ctx.done(err);
-				// Both next called with err AND layers is error handler
-				// Call error handler
-
-				//@ts-ignore
-				if (err && layer.error) return layer.func(err, ctx, input, ...args);
-				// Next called with err OR layers is error handler, but not both
-				// Next layer
-				if (err || layer.error) return ctx.next(err);
-				// Layer is optional and predicate resolves to false
-				// Next layer
-				if (layer.optional && !layer.predicate(ctx, input)) return ctx.next();
-
-				// Call original function handler
-				// @ts-ignore
-				const retVal = await layer.func(ctx, input, ...args);
-				if (retVal instanceof Error) throw retVal;
-				// @ts-ignore
-				else return layer.func(ctx, input, ...args);
-			} catch (e) {
-				return ctx.next(e);
-			}
-		};
-		ctx.next();
-	}
+		if (res instanceof Error) exit();
+	};
 }
 
 export default AzureMiddleMen;
